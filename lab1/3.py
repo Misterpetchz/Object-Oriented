e127fad9833fe1576bdc52c64ab3e50090d30ca9
Misterpetchz <petch.t1507@gmail.com>
2023-01-11 13:17:17 +0700

    Success!

--- lab1/3.py
+++ lab1/3.py
@@ -1,12 +1,14 @@
-
-hr1,m1,hr2,m2 = [int(x) for x in input("Enter your In and Out: ").split()]
+hr1,m1,hr2,m2 = [int(x) for x in input().split()]
+if m2-m1 < 0:
+    m1 += 60
+    hr1 -=1
 dhr = hr2 - hr1
 dm = m2 - m1
 time = (hr2 * 60 - m2) - (hr1 * 60 + m1)
 if time < 15:
     print("0 bant")
     exit()
-if dm > 0:
+if time > 0:
     dhr += 1
 if dhr <= 3:
     paid = dhr * 10
