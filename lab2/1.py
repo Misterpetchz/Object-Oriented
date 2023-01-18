0c822c98ab6af6226b215fbf4fb756f460bec3c3
Misterpetchz <petch.t1507@gmail.com>
2023-01-18 15:16:26 +0700

    toggle

--- lab2/1.py
+++ lab2/1.py
@@ -2,7 +2,5 @@ lists = [int(digit) for digit in input().split()]
 lists.sort()
 for i in range(len(lists)):
     if lists[0] == 0:
-        temp = lists[0]
-        lists[0] = lists[1]
-        lists[i] = temp
+        lists[i],lists[0] = lists[0],lists[i]
 print(*lists, sep="")
