9480524ca2c649badb9e936fa97b1d9717adadec
Misterpetchz <petch.t1507@gmail.com>
2023-01-24 01:31:16 +0700

    Update 5.py

--- lab3/5.py
+++ lab3/5.py
@@ -1,10 +1,22 @@
-def update_records(dictionary_record, id, property, value):
+record_collection = {2548: {'albumTitle': 'Slippery When Wet',
+                            'artist': 'Bon Jovi',
+                            'tracks': ['Let It Rock', 'You Give Love a Bad Name']},
+                     2468: {'albumTitle': '1999',
+                            'artist': 'Prince',
+                            'tracks': ['1999', 'Little Red Corvette']},
+                     1245: {'artist': 'Robert Palmer','tracks': []},
+                     5439: {'albumTitle': 'ABBA Gold'}}
 
-# record_collection = {2548: {'albumTitle': 'Slippery When Wet',
-#                             'artist': 'Bon Jovi',
-#                             'tracks': ['Let It Rock', 'You Give Love a Bad Name']},
-#                      2468: {'albumTitle': '1999',
-#                             'artist': 'Prince',
-#                             'tracks': ['1999', 'Little Red Corvette']},
-#                      1245: {'artist': 'Robert Palmer','tracks': []},
-#                      5439: {'albumTitle': 'ABBA Gold'}}
+def update_records(dictionary_record, id, property, value):
+       if id in dictionary_record.keys():
+              if value == '':
+                     del dictionary_record[id][property]
+              elif (property == 'tracks' and not ('tracks' in record_collection[id])):
+                     dictionary_record[id]['tracks'] = [value]
+              elif (property == 'tracks' and dictionary_record[id][property] != None):
+                     dictionary_record[id]['tracks'].append(value)
+              elif (property != 'tracks'):
+                     dictionary_record[id][property] = value
+       return dictionary_record
+
+print(update_records(record_collection, 5439,'tracks', '2000'))
