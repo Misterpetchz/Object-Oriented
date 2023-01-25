5199fda191a01fcc8d8256ab93ec4acfc624f5a1
Misterpetchz <petch.t1507@gmail.com>
2023-01-25 15:34:21 +0700

    -

--- lab3/2.py
+++ lab3/2.py
@@ -5,7 +5,7 @@ def add_score(subject_score, student, subject, score):
         subject_score.update({student : {subject : score}})
     return subject_score
 
-def calc_average_score(score):
-    return {[x for x in score.keys()][i] : [f"{elem:.2f}" for elem in [sum(score[x].values()) / len(score[x]) for x in score.keys()]][i] for i in range(len(score))}
+def calc_average_score(subject_score):
+    return {id : f'{sum(score for score in subject_score[id].values()) / len(subject_score[id]):.2f}' for id in subject_score}
 print(add_score({ '65010773' : { 'python' : 50 }, '65010780' : {'python': 80, 'calculus': 90} },'65010773','calculus',60))
 print(calc_average_score(add_score({ '65010773' : { 'python' : 50 }, '65010780' : {'python': 80, 'calculus': 90} },'65010773','calculus',60)))
