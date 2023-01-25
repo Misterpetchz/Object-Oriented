fee3d0855d3e117ff91c7cb1e5ec624ac72c6881
Misterpetchz <petch.t1507@gmail.com>
2023-01-25 13:32:50 +0700

    rebuild_format

--- lab3/1.py
+++ lab3/1.py
@@ -3,5 +3,5 @@ def add_score(subject_score, subject, score):
     return subject_score
 
 def calc_average_score(subject_score):
-    return '{:.2f}'.format(sum(subject_score.values())/len(subject_score))
+    return f'{sum(subject_score.values()) / len(subject_score):.2f}'
 print(calc_average_score(add_score({'python' : 50},'calculus',60)))
--- lab3/2.py
+++ lab3/2.py
@@ -6,6 +6,6 @@ def add_score(subject_score, student, subject, score):
     return subject_score
 
 def calc_average_score(score):
-    return {[x for x in score.keys()][i] : ["{:.2f}".format(elem) for elem in [sum(score[x].values()) / len(score[x]) for x in score.keys()]][i] for i in range(len(score))}
+    return {[x for x in score.keys()][i] : [f"{elem:.2f}" for elem in [sum(score[x].values()) / len(score[x]) for x in score.keys()]][i] for i in range(len(score))}
 print(add_score({ '65010773' : { 'python' : 50 }, '65010780' : {'python': 80, 'calculus': 90} },'65010773','calculus',60))
 print(calc_average_score(add_score({ '65010773' : { 'python' : 50 }, '65010780' : {'python': 80, 'calculus': 90} },'65010773','calculus',60)))
